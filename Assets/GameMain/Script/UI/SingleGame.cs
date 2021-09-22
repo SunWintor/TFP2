dee3dbb9ced1a298598dfedf042c5c08248ea0c6
SunWintor <793815374@qq.com>
2021-09-22 23:36:31 +0800

    去除日志

--- Assets/GameMain/Script/UI/SingleGame.cs
+++ Assets/GameMain/Script/UI/SingleGame.cs
@@ -13,21 +13,18 @@ public class SingleGame : UGFR.UIFormLogic {
      * 关闭
      */
     public void CloseButton() {
-        UGFR.Log.Debug("CloseButton");
     }
 
     /**
      * 加
      */
     public void AddButton(int count) {
-        UGFR.Log.Debug("AddButton" + count);
     }
 
     /**
      * 确认
      */
     public void ReadyButton() {
-        UGFR.Log.Debug("ReadyButton");
     }
 
 }
