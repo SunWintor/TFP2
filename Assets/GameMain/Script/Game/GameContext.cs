bab4be9e98d8f9405a4c17608e2cfa5f8a790d45
SunWintor <793815374@qq.com>
2021-09-26 23:27:09 +0800

    model

--- Assets/GameMain/Script/Game/GameContext.cs
+++ Assets/GameMain/Script/Game/GameContext.cs
@@ -2,8 +2,18 @@ public class GameContext {
     public int CurrendRound;
     public int MaxHp;
     public int CurrentHp;
-    public int RoundPayHp;
+    public PayHp RoundPayHp;
     public int PublicDamage;
 
 
+}
+
+public class GameUserInfo {
+    public string userName;
+    public int userId;
+}
+
+public class PayHp {
+    public GameUserInfo payUser;
+    public int payHp;
 }
