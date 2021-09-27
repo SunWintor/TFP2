6068a4b78e57c81c5f8af4fb9b212edcdb37ba0f
SunWintor <793815374@qq.com>
2021-09-27 22:38:19 +0800

    context

--- Assets/GameMain/Script/Game/GameContext.cs
+++ Assets/GameMain/Script/Game/GameContext.cs
@@ -1,19 +1,41 @@
 public class GameContext {
-    public int CurrendRound;
-    public int MaxHp;
-    public int CurrentHp;
-    public PayHp RoundPayHp;
-    public int PublicDamage;
-
-
+    public int CurrendRound {
+        get;
+        set;
+    }
+    public int MaxHp {
+        get;
+        set;
+    }
+    public int CurrentHp {
+        get;
+        set;
+    }
+    public int RoundPayHp {
+        get;
+        set;
+    }
+    public int PublicDamage {
+        get;
+        set;
+    }
+    public GameUserInfo CurrentUser {
+        get;
+        set;
+    }
+    public bool IsReady {
+        get;
+        set;
+    }
 }
 
 public class GameUserInfo {
-    public string userName;
-    public int userId;
-}
-
-public class PayHp {
-    public GameUserInfo payUser;
-    public int payHp;
+    public string UserName {
+        get;
+        set;
+    }
+    public int UserId {
+        get;
+        set;
+    }
 }
--- Assets/GameMain/Script/UI/SingleGame.cs
+++ Assets/GameMain/Script/UI/SingleGame.cs
@@ -5,9 +5,14 @@ using Tutorial;
 public class SingleGame : UGFR.UIFormLogic {
 
     private static ProcedureSingleMode procedureSingleMode;
+    private static GameContext gameContext;
 
     protected override void OnOpen(object userData) {
         procedureSingleMode = (ProcedureSingleMode)userData;
+        gameContext = new GameContext();
+        gameContext.CurrentUser = new GameUserInfo();
+        //gameContext.CurrentUser.UserId = ;
+        //gameContext.CurrentUser.UserId = ;
     }
 
     /**
@@ -22,12 +27,24 @@ public class SingleGame : UGFR.UIFormLogic {
      * 加
      */
     public void AddButton(int count) {
+        // todo 回合结束时，改变状态
+        if (gameContext.IsReady) {
+            return;
+        }
+        gameContext.RoundPayHp += count;
+        if (gameContext.RoundPayHp < 0) {
+            gameContext.RoundPayHp = 0;
+        }
+        if (gameContext.RoundPayHp > gameContext.CurrentHp) {
+            gameContext.RoundPayHp = gameContext.CurrentHp;
+        }
     }
 
     /**
      * 确认
      */
     public void ReadyButton() {
+        gameContext.IsReady = true;
     }
 
 }
